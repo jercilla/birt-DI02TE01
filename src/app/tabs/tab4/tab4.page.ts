1b2337ef322caad6e54971b65b739d0d572af69e
jercilla <joseba.ercilla@gmail.com>
2023-11-30 19:15:50 +0100

    TAB$ número aleatorio

--- src/app/tabs/tab4/tab4.page.ts
+++ src/app/tabs/tab4/tab4.page.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tab4.page.scss'],
 })
 export class Tab4Page implements OnInit {
-
-  constructor() { }
+  adivina: number | undefined = undefined;
+  respuesta: number | undefined = undefined;
+  resultado: string = 'Introduce un número entre 0 y 100';
+  constructor() {}
 
   ngOnInit() {
+    this.adivina = Math.round(Math.random() * 101);
   }
 
+  onClick() {
+    if (this.respuesta == this.adivina) {
+      this.resultado = 'Has acertado';
+    } else {
+      if (this.respuesta! < this.adivina!) {
+        this.resultado = 'Introduce un número mayor';
+      } else {
+        this.resultado = 'Introduce un número menor';
+      }
+    }
+  }
 }
